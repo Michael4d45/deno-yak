113009fce3cd7719f88734ec474d7dbaae413e60
Michael J Harvey <michael4d45@gmail.com>
2023-03-03 20:39:00 -0700

    move yak logic to yak folder

---
+++ Yak/useYak.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "preact/hooks";
+import useAST from "./AST/useAST.ts";
+import useCompute from "./Compute/useCompute.ts";
+import tokenizer from "./Tokenizer/index.ts";
+import { Token } from "./types.ts";
+
+const useYak = () => {
+  const [tokens, setTokens] = useState<Token[]>([]);
+  const [tokenErrors, setTokenErrors] = useState<Token[]>([]);
+  const [pos, setPos] = useState(1);
+
+  const onSubmit = (value: string) => {
+    setTokens(tokenizer(value));
+  };
+
+  const block = useAST({ tokens });
+
+  const {
+    setBlock,
+    stack,
+    step,
+    run,
+    calcError,
+    running,
+    reset,
+    fast,
+    variables,
+  } = useCompute({
+    setPos,
+  });
+
+  useEffect(() => {
+    const newTokenErrors = tokens.filter((token: Token) => token.errors.length > 0);
+
+    setTokenErrors(newTokenErrors);
+
+    if (newTokenErrors.length === 0) {
+      setBlock(block);
+    }
+  }, [tokens, block]);
+
+  return {
+    tokens,
+    tokenErrors,
+    pos,
+    onSubmit,
+    stack,
+    step,
+    run,
+    calcError,
+    running,
+    reset,
+    fast,
+    variables,
+  }
+}
+
+export default useYak;
--- islands/PlayGround.tsx
+++ islands/PlayGround.tsx
@@ -1,78 +1,46 @@
-import { useEffect, useState } from "preact/hooks";
 import Input from "../components/Input.tsx";
 import Tokens from "../components/Tokens.tsx";
-import useCompute from "../Yak/Compute/useCompute.ts";
-import tokenizer from "../Yak/Tokenizer/index.ts";
-import { Token } from "../Yak/types.ts";
-import useAST from "../Yak/AST/useAST.ts";
 import { Variable } from "../Yak/Compute/types.ts";
+import useYak from "../Yak/useYak.ts";
 
 const PlayGround = () => {
-  const [tokens, setTokens] = useState<Token[]>([]);
-  const [tokenErrors, setTokenErrors] = useState<Token[]>([]);
-  const [pos, setPos] = useState(1);
-
-  const onSubmit = (value: string) => {
-    setTokens(tokenizer(value));
-  };
-
-  const block = useAST({ tokens });
-
-  const {
-    setBlock,
-    stack,
-    step,
-    run,
-    calcError,
-    running,
-    reset,
-    fast,
-    variables,
-  } = useCompute({
-    setPos,
-  });
-
-  useEffect(() => {
-    const newTokenErrors = tokens.filter((token) => token.errors.length > 0);
-
-    setTokenErrors(newTokenErrors);
-
-    if (newTokenErrors.length === 0) {
-      setBlock(block);
-    }
-  }, [tokens, block]);
+  const yak = useYak();
 
   return (
     <div class="flex flex-row w-screen h-screen font-mono">
       <div class="w-1/3 bg-gray-100">
-        <Input {...{ onSubmit }} />
+        <Input onSubmit={yak.onSubmit} />
       </div>
       <div class="w-1/3 bg-gray-200 whitespace-pre h-full flex flex-col">
         <div class="flex-grow overflow-y-scroll">
-          <Tokens tokens={tokens} tokenErrors={tokenErrors} pos={pos} />
+          <Tokens
+            tokens={yak.tokens}
+            tokenErrors={yak.tokenErrors}
+            pos={yak.pos}
+          />
         </div>
         <div class="w-full flex flex-row">
           <button
             class="flex-grow border border-2 border-gray-300 rounded-lg"
-            onClick={step}
+            onClick={yak.step}
           >
             Step
           </button>
           <button
             class="flex-grow border border-2 border-gray-300 rounded-lg"
-            onClick={run}
+            onClick={yak.run}
           >
-            {running === null ? <>Run</> : <>Stop</>}
+            {yak.running === null ? <>Run</> : <>Stop</>}
           </button>
           <button
             class="flex-grow border border-2 border-gray-300 rounded-lg"
-            onClick={fast}
+            onClick={yak.fast}
           >
-            {running === null ? <>Fast</> : <>Stop</>}
+            {yak.running === null ? <>Fast</> : <>Stop</>}
           </button>
           <button
             class="flex-grow border border-2 border-gray-300 rounded-lg"
-            onClick={reset}
+            onClick={yak.reset}
           >
             Reset
           </button>
@@ -81,14 +49,14 @@ const PlayGround = () => {
       <div class="w-1/3 bg-gray-300 flex flex-row space-x-2">
         <div class="flex flex-col">
           <div>Stack</div>
-          {calcError && <div class="text-red-500">{calcError}</div>}
+          {yak.calcError && <div class="text-red-500">{yak.calcError}</div>}
           <div class="overflow-y-scroll">
-            {[...stack].reverse().map((num, i) => (
+            {[...yak.stack].reverse().map((num, i) => (
               <div key={i}>{num}</div>
             ))}
           </div>
         </div>
-        {variables.map(({ name, stack }: Variable) => (
+        {yak.variables.map(({ name, stack }: Variable) => (
           <div class="flex flex-col">
             <div>{name}</div>
             <div class="overflow-y-scroll">
