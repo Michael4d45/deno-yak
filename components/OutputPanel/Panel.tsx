af016f022fdd07bbedf4f37ae7003ed4de26b5e8
Michael J Harvey <michael4d45@gmail.com>
2023-03-05 00:24:50 -0700

    add console to 3rd panel

--- components/OutputPanel/Panel.tsx
+++ components/OutputPanel/Panel.tsx
@@ -1,27 +1,33 @@
 import { Variable } from "../../Yak/Compute/types.ts";
 import { YakType } from "../../Yak/useYak.ts";
+import Console from "./Console.tsx";
 
 const OutputPanel = ({ calcError, stack, variables }: YakType) => (
-  <div class="w-1/3 bg-gray-300 flex flex-row space-x-2">
-    <div class="flex flex-col">
-      <div>Stack</div>
-      {calcError && <div class="text-red-500">{calcError}</div>}
-      <div class="overflow-y-scroll">
-        {[...stack].reverse().map((num, i) => (
-          <div key={i}>{num}</div>
-        ))}
-      </div>
-    </div>
-    {variables.map(({ name, stack }: Variable) => (
+  <div class="w-1/3 bg-gray-300 flex flex-col">
+    <div class="flex flex-row space-x-2 h-1/2">
       <div class="flex flex-col">
-        <div>{name}</div>
+        <div>Stack</div>
+        {calcError && <div class="text-red-500">{calcError}</div>}
         <div class="overflow-y-scroll">
           {[...stack].reverse().map((num, i) => (
             <div key={i}>{num}</div>
           ))}
         </div>
       </div>
-    ))}
+      {variables.map(({ name, stack }: Variable) => (
+        <div class="flex flex-col">
+          <div>{name}</div>
+          <div class="overflow-y-scroll">
+            {[...stack].reverse().map((num, i) => (
+              <div key={i}>{num}</div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+    <div class="h-1/2">
+      <Console />
+    </div>
   </div>
 );
 
