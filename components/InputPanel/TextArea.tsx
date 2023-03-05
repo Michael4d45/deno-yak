18095815732c9c9275e6c4304ca9dbca603af119
Michael J Harvey <michael4d45@gmail.com>
2023-03-05 00:09:55 -0700

    add fib variant

--- components/InputPanel/TextArea.tsx
+++ components/InputPanel/TextArea.tsx
@@ -1,17 +1,134 @@
 import { useEffect, useRef } from "preact/hooks";
 
-const TextArea = () => {
-  const inputRef = useRef<HTMLTextAreaElement>(null);
+const ITERATE=`
 
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
+[]n
+[]i
+0#iterate {
+    // for i in range(n - 1):
+    1 <-n . 1 ->n
+    1 <-i . 1 + 1 ->i
+    == ! ? {
+        // a, b = b, a + b
+        // (ba -- bab)
+        <^>
+        
+        // (bab -- (b + a)b)
+        + 
+
+        iterate
     }
-  });
+}
 
-  return (
-    <textarea class="w-full h-full">
-      {`1#fib
+
+1#fib {
+    // if n <= 1:
+    . 1 <= . ? { @
+        // return n
+    1 } ! ? {
+        1 - 1 ->n
+        // a, b = 0, 1
+        0
+        1
+        
+        0 1 ->i
+        iterate
+
+        // return b
+    }
+}
+
+50 . fib
+
+"Fib({3})={1}"
+
+`
+
+const MEMO = `
+[]n
+[]memo
+[]memo_index
+
+// fib(0) = 1
+0 1 ->memo_index
+0 1 ->memo
+// fib(1) = 1
+1 1 ->memo_index
+1 1 ->memo
+
+// (n,m -- <1|0>n,m)
+1#IS_MEMOIZED 
+{
+  .
+  1 <-memo_index
+  . 1 ->memo_index
+  <=
+}
+
+// (n,fib(n),a, -- fib(n),a,n)
+2#MEMOIZE
+{
+ IS_MEMOIZED ! . ? { @ 
+  1 ->memo_index
+  . 1 ->memo
+  1 } ! ? {
+    @
+  }
+}
+
+[]check
+[]temp_memo
+[]temp_memo_index
+// ( ,c,a[] -- n,c,a[])
+0#GET_ME {
+  1 <-memo
+  1 ->temp_memo
+  1 <-memo_index
+  .
+  1 ->temp_memo_index
+  1 <-check
+  . 1 ->check
+  == . ? { @
+    1 <-temp_memo
+    . 1 ->temp_memo
+  1 } ! ? {
+    GET_ME
+  }
+  1 <-temp_memo
+  1 ->memo
+  1 <-temp_memo_index
+  1 ->memo_index
+}
+
+// where a[] is [a_x...a_n...a_0]
+// (n,a[] -- a_n,a[])
+1#GET_MEMO 
+{
+  1 ->check
+  GET_ME
+  1 <-check @
+}
+
+// (n -- fib(n))
+1#FIB 
+{
+  IS_MEMOIZED . ? { @
+    GET_MEMO
+  1 } ! ? {
+    . 1 ->n
+    . 1 - FIB 
+    <-> 2 - FIB
+    +
+    1 <-n
+    MEMOIZE
+  }
+}
+
+55 FIB
+
+`
+
+const FIB = `1#fib
 {
   . 1 ->MY_VAR
   . . 1 == <-> 0 == | ! ?
@@ -30,7 +147,20 @@ const TextArea = () => {
 
 
 2
-<-MY_VAR`}
+<-MY_VAR`
+
+const TextArea = () => {
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  });
+
+  return (
+    <textarea class="w-full h-full">
+      {ITERATE}
     </textarea>
   );
 };
