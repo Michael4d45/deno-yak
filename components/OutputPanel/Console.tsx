1a39755067d609e5403132c021061384e68624b1
Michael J Harvey <michael4d45@gmail.com>
2023-03-05 21:07:07 -0700

    scroll the console

--- components/OutputPanel/Console.tsx
+++ components/OutputPanel/Console.tsx
@@ -1,19 +1,26 @@
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useState, useRef } from "preact/hooks";
 import { useBuffer } from "../../OutputBuffer/useBuffer.ts";
 
 const Console = () => {
   const [lines, setLines] = useState<string[]>([]);
+  const consoleRef = useRef<HTMLDivElement>(null);
 
   const onWrite = (line: string) => {
-    setLines([...lines, line])
-  }
+    setLines((lines) => [...lines, line]);
+  };
+
+  useEffect(() => {
+    if (consoleRef.current) {
+      consoleRef.current.scrollTop = consoleRef.current.scrollHeight;
+    }
+  }, [lines]);
 
   useBuffer(onWrite);
 
   return (
-    <div>
-      {lines.map((line) => (
-        <div>{line}</div>
+    <div ref={consoleRef} class="overflow-y-auto h-full">
+      {lines.map((line, index) => (
+        <div key={index}>{line}</div>
       ))}
     </div>
   );
--- components/OutputPanel/Panel.tsx
+++ components/OutputPanel/Panel.tsx
@@ -3,7 +3,7 @@ import { YakType } from "../../Yak/useYak.ts";
 import Console from "./Console.tsx";
 
 const OutputPanel = ({ calcError, stack, variables }: YakType) => (
-  <div class="w-1/3 bg-gray-300 flex flex-col">
+  <div class="w-1/3 bg-gray-300 flex flex-col h-screen">
     <div class="flex flex-row space-x-2 h-1/2">
       <div class="flex flex-col">
         <div>Stack</div>
